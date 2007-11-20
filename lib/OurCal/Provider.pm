114c630a096495a874c4b4dce782e0f74e4fd7e4
simon <simon@c56e198b-7db7-0310-b3ab-8e406f684e33>
2007-11-20 15:05:59 +0000

    Switch to new, better, provider loading

--- lib/OurCal/Provider.pm
+++ lib/OurCal/Provider.pm
@@ -2,67 +2,76 @@ package OurCal::Provider;
 
 use strict;
 use UNIVERSAL::require;
+use Module::Pluggable sub_name    => '_providers',
+                      search_path => 'OurCal::Provider';
 
 sub new {
     my $class = shift;
     my %what  = @_;
-    my $conf  = $what{config}->config('providers');
-    my %providers;
-    foreach my $name (keys %$conf) {
-        my $what     = $conf->{$name};
-        my $provider = $class."::".ucfirst(lc($what));
-        $provider->require || die "Couldn't require class $provider: $@\n";
-        $providers{$name} = $provider->new( config => $what{config}, name => $name );
+
+    # first work out what provider we're using
+    my $conf     = $what{config};
+    my @args ;
+    my $name     = $conf->{_}->{provider};
+    if (defined $name) {
+        push @args, $name;
+    } else {
+        push @args, ("providers", $conf, type => "multi");
     }
-    die "You must provide a provider named 'default'\n" unless defined $providers{'default'};
-    $what{_providers} = { %providers };
+    # then load it
+    $what{_provider} = $class->load_provider(@args); 
     return bless \%what, $class;
 }
 
+
 sub providers {
-    my $self = shift;
-    return values %{$self->{_providers}};
+    my $self  = shift;
+    my $class = (ref $self)? ref($self) : $self;
+
+    my %providers;
+    foreach my $provider ($self->_providers) {
+        my $name = $provider;
+        $name =~ s!^${class}::!!;
+        $providers{lc($name)} = $provider;
+    }
+    return %providers;
 }
+
+sub load_provider {
+    my $self  = shift;
+    my $name  = shift;
+    my $conf  = shift;  
+    my %opts  = @_;
+    my $pconf = $conf->config($name) || die "Don't know about provider $name\n";
+    my $type  = $pconf->{type}       || $opts{type}  || die "Couldn't work out type for provider $name - you must provide a 'type' config\n"; 
+    my %provs = $self->providers;
+    my $class = $provs{lc($type)}    || die "Couldn't get a class for provider $name of type $type\n";
+    $class->require || die "Couldn't require class $class: $@\n";
+    return $class->new(config => $conf, name => $name);
+}
+
 
 sub todos {
     my $self = shift;
-    return $self->_gather('todos', @_);
+    return $self->_do_default('todos', @_);
 }
 
 sub has_events {
     my $self = shift;
-    my %opts = @_;
-    foreach my $provider ($self->providers) {
-        return 1 if $provider->has_events(%opts);
-    }
-    return 0;
+    return $self->_do_default('has_events', @_);
 }
 
 sub events {
     my $self   = shift;
     my %opts   = @_;
-    my @events = sort { $b->date cmp $a->date } $self->_gather('events', %opts);
+    my @events = sort { $b->date cmp $a->date } $self->_do_default('events', %opts);
     @events    = splice @events, 0, $opts{limit} if defined $opts{limit};
     return @events;
 }
 
 sub users {
     my $self = shift;
-    return $self->_gather('users', @_);
-}
-
-
-sub _gather {
-    my $self = shift;
-    my $sub  = shift;
-    my %opts = @_;
-
-    my @vals;
-    foreach my $provider ($self->providers) {
-        push @vals, $provider->$sub(%opts);
-    }
-    return @vals;
-
+    return $self->_do_default('users', @_);
 }
 
 sub save_todo {
@@ -90,8 +99,7 @@ sub _do_default {
     my $self  = shift;
     my $sub   = shift;
     my $thing = shift;
-    $self->{_providers}->{default}->$sub($thing);
+    $self->{_provider}->$sub($thing, @_);
 }
-
 1;
 
---
+++ lib/OurCal/Provider/Multi.pm
@@ -0,0 +1,98 @@
+package OurCal::Provider::Multi;
+
+use strict;
+use OurCal::Provider;
+
+sub new {
+    my $class = shift;
+    my %what  = @_;
+
+    # get all the names of of our providers
+    my $conf      = $what{config}->config($what{name});
+    my $providers = $conf->{providers};
+    my $default   = 0;
+    foreach my $provider (split ' ', $providers) {
+        $what{_providers}->{$provider} = OurCal::Provider->load_provider($provider, $what{config});        
+        $default = 1 if $provider eq 'default';
+    }
+    return bless \%what, $class;
+}
+
+sub todos {
+    my $self = shift;
+    return $self->_gather('todos', @_);
+}
+
+sub providers {
+    my $self = shift;
+    return values %{$self->{_providers}};
+}
+
+sub has_events {
+    my $self = shift;
+    my %opts = @_;
+    foreach my $provider ($self->providers) {
+        return 1 if $provider->has_events(%opts);
+    }
+    return 0;
+}
+
+sub events {
+    my $self   = shift;
+    my %opts   = @_;
+    my @events = sort { $b->date cmp $a->date } $self->_gather('events', %opts);
+    @events    = splice @events, 0, $opts{limit} if defined $opts{limit};
+    return @events;
+}
+
+sub users {
+    my $self = shift;
+    return $self->_gather('users', @_);
+}
+
+
+sub _gather {
+    my $self = shift;
+    my $sub  = shift;
+    my %opts = @_;
+
+    my @vals;
+    foreach my $provider ($self->providers) {
+        push @vals, $provider->$sub(%opts);
+    }
+    return @vals;
+
+}
+
+sub save_todo {
+    my $self = shift;
+    return $self->_do_default('save_todo', @_);
+}
+
+sub del_todo {
+    my $self = shift;
+    return $self->_do_default('del_todo', @_);
+}
+
+
+sub save_event {
+    my $self = shift;
+    return $self->_do_default('save_event', @_);
+}
+
+sub del_event {
+    my $self = shift;
+    return $self->_do_default('del_event', @_);
+}
+
+sub _do_default {
+    my $self  = shift;
+    my $sub   = shift;
+    my $thing = shift;
+    die "You must specify at least one provider named 'default' if you want to save\n" 
+        unless $self->{_providers}->{default};
+    $self->{_providers}->{default}->$sub($thing);
+}
+
+1;
+
