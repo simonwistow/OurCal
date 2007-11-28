80fb9b2518e89250dfd10b338dfb2e15ad053db8
simon <simon@c56e198b-7db7-0310-b3ab-8e406f684e33>
2007-11-28 15:49:05 +0000

    tidy

--- calendar.sql
+++ calendar.sql
@@ -1,15 +1,14 @@
 
 CREATE TABLE events (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY_KEY,
-	date VARCHAR(10) NOT NULL,
-	description TEXT NOT NULL,
+    date VARCHAR(10) NOT NULL,
+    description TEXT NOT NULL,
     user TEXT, 
-
 );
 
 CREATE todos (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY_KEY,
-	description TEXT NOT NULL
+    description TEXT NOT NULL
     user TEXT, 
 );
 
